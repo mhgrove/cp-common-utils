8084c0a1ff7f9eea8061c463d339456f7fc921b0
mikeg <mikeg@e81fe1a9-c818-0410-a7be-a7cfb6fb3625>
2009-05-14 14:37:32 +0000

    * getting jspace back up to compilability, mainly just matching changes i've made in the sesame and common utils stuff since i was last in this code.
    git-svn-id: http://clark-parsia.svn.cvsdude.com/swlib/common/utils/trunk@197 e81fe1a9-c818-0410-a7be-a7cfb6fb3625

--- src/com/clarkparsia/utils/CollectionUtil.java
+++ src/com/clarkparsia/utils/CollectionUtil.java
@@ -6,10 +6,11 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Collection;
 
 /**
- * Title: <br/>
- * Description: <br/>
+ * Title: CollectionUtil<br/>
+ * Description: Utility methods for using and working with collections.<br/>
  * Company: Clark & Parsia, LLC. <http://www.clarkparsia.com> <br/>
  * Created: Apr 21, 2009 10:15:38 AM <br/>
  *
@@ -48,7 +49,7 @@ public class CollectionUtil {
      * @param toSearch Set the search set
      * @return boolean true if any element in the first set is present in the second
      */
-    public static <T> boolean containsAny(Set<T> theList, Set<T> toSearch) {
+    public static <T> boolean containsAny(Collection<T> theList, Collection<T> toSearch) {
 
         if (toSearch.isEmpty()) {
             return false;
@@ -64,7 +65,77 @@ public class CollectionUtil {
         }
     }
 
-    public static <T> boolean containsAny(List<T> theList, List<T> theSearch) {
-        return containsAny(new HashSet<T>(theList), new HashSet<T>(theSearch));
+	    /**
+     * Convienence method for creating a set that is the union of N other sets
+     * @param theSets the sets to create a union of
+     * @param <T> the type of the sets
+     * @return a set which is the union of all the passed in sets
+     */
+    public static <T> Set<T> union(Set<T>... theSets) {
+        Set<T> aUnion = new HashSet<T>();
+
+        for (Set<T> aSet : theSets) {
+            aUnion.addAll(aSet);
+        }
+
+        return aUnion;
+    }
+
+    /**
+     * Convienence method for creating a set that is the intersection of all N sets
+     * @param theSets the sets to create the intersection from
+     * @param <T> the type of the sets
+     * @return a new set which is the intersection of the provided sets
+     */
+    public static <T> Set<T> intersection(Set<T>... theSets) {
+        Set<T> aIntersection = new HashSet<T>();
+
+        boolean aFirst = true;
+        for (Set<T> aSet : theSets) {
+            if (aFirst) {
+                aFirst = false;
+                aIntersection.addAll(aSet);
+            }
+            else {
+                aIntersection.retainAll(aSet);
+            }
+        }
+
+        return aIntersection;
+    }
+
+    /**
+     * Returns true if two collections have the same contents.  This is used to compare two collections which may or may
+     * not be the same type, such as comparing a list and a set, which normally will not be equals even if they
+     * contain the same set of values.  This does an equals comparision on the contents of the collections.
+     * @param theList the first collection to compare
+     * @param theCompare the second collection to compare
+     * @param <T> the object type in the collections
+     * @return true if they have the same contents, false otherwise
+     */
+    public static <T> boolean contentsEqual(Collection<T> theList, Collection<T> theCompare) {
+        if (theList.size() != theCompare.size()) {
+            return false;
+        }
+
+        for (T aListElem : theList) {
+            Iterator aCompareIter = theCompare.iterator();
+
+            boolean found = false;
+            while (aCompareIter.hasNext()) {
+                Object aCompareItem = aCompareIter.next();
+
+                if (aListElem.equals(aCompareItem)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
