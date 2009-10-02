a9b6aff10e2d72e00c2b70d59c3149332ed53fbb
mikeg <mikeg@e81fe1a9-c818-0410-a7be-a7cfb6fb3625>
2009-10-02 16:08:44 +0000

    moved a couple normal utils stuff into the basic utils for doing hex and md5 of a string
    git-svn-id: http://clark-parsia.svn.cvsdude.com/swlib/common/utils/trunk@225 e81fe1a9-c818-0410-a7be-a7cfb6fb3625

--- src/com/clarkparsia/utils/BasicUtils.java
+++ src/com/clarkparsia/utils/BasicUtils.java
@@ -8,6 +8,9 @@ import java.util.Date;
 
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Collection of simple utility methods
@@ -149,5 +152,38 @@ public class BasicUtils {
 
     public static String datetime(Date theDate) {
         return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(theDate);
+    }
+
+	/**
+     * Return the hex string value of the byte array
+	 * @param theArray the input array
+	 * @return the hex value of the bytes
+	 */
+	public static String hex(byte[] theArray) {
+		StringBuffer sb = new StringBuffer();
+		for (byte aByte : theArray) {
+			sb.append(Integer.toHexString((aByte & 0xFF) | 0x100).toUpperCase().substring(1, 3));
+		}
+		return sb.toString();
+	}
+
+	/**
+     * Returns the md5 representation of a string
+     * @param theString the string to md5
+     * @return the byte representation of the md5 sum of the string
+     */
+    public static byte[] md5(String theString) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            return md.digest(theString.getBytes("UTF-8"));
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
